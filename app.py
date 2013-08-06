2b2d7ac80f8b1c7c6e11144d30e968c26fa86bde
Riya Modi <12rmodi@castilleja.org>
2013-08-06 15:44:03 -0700

    Added dorm layouts and machine images

--- app.py
+++ app.py
@@ -8,12 +8,6 @@ app = Flask(__name__)
 #testing ability to get signal from Twine
 @app.route("/", methods=['GET','POST'])
 def index():
-	# print "at beggining of function"
-
-	# vibration_status=request.form["vibration"]
-	# print "vibration_status is", vibration_status
-
-	# return "did it work?"
 
 	return render_template("homepage.html")
 
@@ -30,6 +24,9 @@ def new_load():
 	#############
 	#do i need to commit the following line to session??
 	l.machine.in_use = request.args.get("vibration")
+	model.session.add(machine)
+	model.session.commit()
+	model.session.refresh(machine)
 	#should be a better way of doing this
 	l.machine.in_use = l.machine.in_use.replace('[','')
 	l.machine.in_use = l.machine.in_use.replace(']','')
@@ -41,7 +38,9 @@ def new_load():
 	print "specific location: ", l.machine.location.school,l.machine.location.dorm,l.machine.location.floor
 	all_machines = model.session.query(model.Machine).filter_by(location_id=l.machine.location_id).all()
 	print "all_machines: ", all_machines
-
+	for m in all_machines:
+		print "status: ", m.in_use
+	
 	
 	#I want the html template that I return to have variables within the html name
 	#ex: [l.machine.location.school+l.machine.location.dorm+l.machine.location.floor].html
@@ -54,19 +53,19 @@ def new_load():
 	underscore_dorm = l.machine.location.dorm.replace(' ', '_')
 	print "new dorm: ", underscore_dorm
 
-	return render_template("%s%s%d.html" %(underscore_school,underscore_dorm,l.machine.location.floor), 
-							load=l,machines=all_machines)
-
-	#****if it sends a request every second taht it is vibrating,
-	#i need a way of only counting the first one
-	# print "machine type: ", type(machine)
-	# print "l.machine_id.machine type", type(l.machine_id.machine )
-	# print "l.machine", type(l.machine)
-
-
-	#MVP: get status, get id. find location id. display the room.
+	return render_template("%s%s.html" %(underscore_school,underscore_dorm),load=l,machines=all_machines) 
 	
 
-
+	#************************************************************************
+	#************************************************************************
+	#use this return if I want to create a different html file for each floor
+	#for now, I'm going to assume the layout of the laundry room on each floor 
+	#is the same within one dorm
+	
+	# return render_template("%s%s%d.html" %(underscore_school,underscore_dorm,l.machine.location.floor), 
+	# 						load=l,machines=all_machines)
+	#************************************************************************
+	#************************************************************************
+	
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',debug=True)
